afda4a59259cc0448d3b337ce7803855d661646b
nasrwihtw <nasr.al-sayaghi@Student.HTW.Berlin.de>
2024-07-08 15:11:57 +0200

    Socket url updated

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -14,7 +14,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8000", {
+      const socket = io("https://chat-app-se1j.onrender.com", {
         query: { 
             userId: authUser._id 
         },
